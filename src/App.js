1a17f6f4669440165c0f098f0516bd1b708e8b14
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-05-04 23:30:38 +0530

    My data3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Joinus from "./pages/Joinus/Joinus";
 import Login from "./pages/Login/Login";
 import Trainers from "./pages/Aboutus/Trainer/Trainers";
-import Activity2 from "./pages/MyActivity2";
 import MyActivity1 from "./pages/MyActivity1";
+import MyActivity2 from "./pages/MyActivity2";
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/trainers" element={<Trainers />}></Route>
         <Route path="/activity1" element={<MyActivity1/>} ></Route>
-        <Route path="/activity2" element={<Activity2/>} ></Route>
+        <Route path="/activity2" element={<MyActivity2/>} ></Route>
       </Routes>
     </Router>
   );
